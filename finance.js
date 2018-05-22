874f786d402a3ccfa9f99ea69015d12ae288197a
oxxo <oxxo.studio@gmail.com>
2018-05-23 01:10:02 +0800

    Fixed bug

--- finance.js
+++ finance.js
@@ -13,21 +13,17 @@
 
   function finance_stock_data(name, type, e) {
     let result = e[0];
-    console.log(name);
-    console.log(result);
+    let reply = 0;
     if (e[1] == 'stock') {
-          console.log(result[2].name);
-          console.log(result[2].uid);
       for (let i = 0; i < result.length; i++) {
-        if (i == 2) {
-          console.log(result[i].name);
-          console.log(result[i].uid);
-        }
         if (result[i].name == name || result[i].uid == name) {
-          return result[i][type];
-        } else {
-          return '查無資料';
+          reply = result[i][type];
         }
+      }
+      if (reply == 0) {
+        return '查無資料';
+      } else {
+        return reply;
       }
     } else {
       return '資料格式錯誤';
