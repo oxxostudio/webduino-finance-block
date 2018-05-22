96a5396b65b3f506b489e478dd072e0775caef80
oxxo <oxxo.studio@gmail.com>
2018-05-22 21:37:00 +0800

    edit name

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -52,25 +52,25 @@ Blockly.Blocks['finance_rate_data'] = {
     this.appendDummyInput()
       .appendField("幣別")
       .appendField(new Blockly.FieldDropdown([
-        ["美金 ( USD )", "美金(USD)"],
-        ["港幣 ( HKD )", "港幣(HKD)"],
-        ["英鎊 ( GBP )", "英鎊(GBP)"],
-        ["澳幣 ( AUD )", "澳幣(AUD)"],
-        ["加拿大幣 ( CAD )", "加拿大幣(CAD)"],
-        ["新加坡幣 ( SGD )", "新加坡幣(SGD)"],
-        ["瑞士法郎 ( CHF )", "瑞士法郎(CHF)"],
-        ["日圓 ( JPY )", "日圓(JPY)"],
-        ["南非幣 ( ZAR )", "南非幣(ZAR)"],
-        ["瑞典幣 ( SEK )", "瑞典幣(SEK)"],
-        ["紐元 ( NZD )", "紐元(NZD)"],
-        ["泰幣 ( THB )", "泰幣(THB)"],
-        ["菲國比索 ( PHP )", "菲國比索(PHP)"],
-        ["印尼幣 ( IDR )", "印尼幣(IDR)"],
-        ["歐元 ( EUR )", "歐元(EUR)"],
-        ["韓元 ( KRW )", "韓元(KRW)"],
-        ["越南盾 ( VND )", "越南盾(VND)"],
-        ["馬來幣 ( MYR )", "馬來幣(MYR)"],
-        ["人民幣 ( CNY )", "人民幣(CNY)"]
+        ["美金 - USD", "USD"],
+        ["港幣 - HKD", "HKD"],
+        ["英鎊 - GBP", "GBP"],
+        ["澳幣 - AUD", "AUD"],
+        ["加拿大幣 - CAD", "CAD"],
+        ["新加坡幣 - SGD", "SGD"],
+        ["瑞士法郎 - CHF", "CHF"],
+        ["日圓 - JPY", "JPY"],
+        ["南非幣 - ZAR", "ZAR"],
+        ["瑞典幣 - SEK", "SEK"],
+        ["紐元 - NZD", "NZD"],
+        ["泰幣 - THB", "THB"],
+        ["菲國比索 - PHP", "PHP"],
+        ["印尼幣 - IDR", "IDR"],
+        ["歐元 - EUR", "EUR"],
+        ["韓元 - KRW", "KRW"],
+        ["越南盾 - VND", "VND"],
+        ["馬來幣 - MYR", "MYR"],
+        ["人民幣 - CNY", "CNY"]
       ]), "name")
       .appendField("類別")
       .appendField(new Blockly.FieldDropdown([
