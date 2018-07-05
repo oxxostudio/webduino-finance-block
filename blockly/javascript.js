8cc980f8eb2e955081a824679c8dbff54bff9d0d
oxxo <oxxo.studio@gmail.com>
2018-07-05 10:50:05 +0800

    Fixed bug

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -9,7 +9,7 @@ Blockly.JavaScript['finance_stock_data'] = function(block) {
   var name = Blockly.JavaScript.valueToCode(block, 'name', Blockly.JavaScript.ORDER_ATOMIC);
   var type = block.getFieldValue('type');
   var code = 'finance_stock_data(' + name + ',"' + type + '",data)';
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 Blockly.JavaScript['finance_rate'] = function(block) {
@@ -24,5 +24,5 @@ Blockly.JavaScript['finance_rate_data'] = function(block) {
   var name = block.getFieldValue('name');
   var type = block.getFieldValue('type');
   var code = 'finance_rate_data("' + name + '","' + type + '",data)';
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
